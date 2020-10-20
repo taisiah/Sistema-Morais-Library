848587f468c567b5a2a8396b279ec74522ceef4c
taisiah <taisiahonorio@gmail.com>
2020-10-20 15:10:09 -0300

    classe ususario e funcionario

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -1,5 +1,20 @@
 package modelo;
 
 public class Usuario {
-
+	private String login;
+	private String senha;
+	private String nome;
+	private String endereço;
+	private String email;
+	private String telefone;
+	
+	
+	
+	public Usuario() {
+		
+	}
+	
+	public Usuario() {
+	}
+		
 }
