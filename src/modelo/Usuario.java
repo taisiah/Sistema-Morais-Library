aee114922be73a17e06224a9fddd11836fab4b40
taisiah <taisiahonorio@gmail.com>
2020-10-27 19:19:28 -0300

    modelo

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -1,20 +1,69 @@
 package modelo;
 
+//atributos
 public class Usuario {
 	private String login;
 	private String senha;
 	private String nome;
-	private String endereço;
+	private String endereco;
 	private String email;
 	private String telefone;
 	
 	
-	
-	public Usuario() {
+	//construtor com campos
+	public Usuario(String login,String senha,String nome,String endereco,String email,String telefone) {
+		this.login = login ;
+		this.senha = senha ;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.email = email;
+		this.telefone = telefone;
 		
 	}
+	//construtor sem campos
 	
-	public Usuario() {
+	
+	public String toString() {
+		return "Usuario [login=" + login + ", senha=" + senha + ", nome=" + nome + ", endereco=" + endereco + ", email="
+				+ email + ", telefone=" + telefone + "]";
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
 	}
 		
+	
+	
 }
